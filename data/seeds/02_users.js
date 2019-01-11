f99190ac80c81c52c8a5ea97e92778bb82292e50
Eva <evalee904@gmail.com>
2019-01-11 02:12:04 -0600

    Remake db

--- data/seeds/02_users.js
+++ data/seeds/02_users.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
 		.then(function() {
 			// Inserts seed entries
 			return knex('users').insert([
-				{ id: 1, username: 'user1', password: 'password1' }
+				{ id: 1, username: 'admin', password: 'admin' }
 			]);
 		});
 };
