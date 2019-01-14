58cf898cae840e60a99e1368d091af57f886ab31
Eva <evalee904@gmail.com>
2019-01-14 04:39:35 -0600

    Updated tests

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -8,23 +8,27 @@ describe('server', () => {
 			expect(response.status).toBe(200);
 		});
 
-		it('should return JSON', async () => {
+		it('should return Hi', async () => {
 			const response = await request(server).get('/');
-			expect(response.type).toBe('application/json');
+			expect(response.type).toBe('text/html');
 		});
 	}); // get
 
-	describe('POST /api/notes/:id', () => {
+	describe('POST /api/notes', () => {
 		it('should return status code 201', async () => {
-			const response = await request(server).post(`/api/notes/${id}`);
+			const title = 'testing';
+			const response = await request(server)
+				.post(`/api/notes`)
+				.send({ title });
 			expect(response.status).toBe(201);
 		});
 	}); // post
 
 	describe('DELETE /api/notes/:id', () => {
 		it('should return status code 200', async () => {
+			const id = 1;
 			const response = await request(server).delete(`/api/notes/${id}`);
-			expect(response.status).toBe(200);
+			expect(response.status).toBe(202);
 		});
 	}); // delete
 }); // server
